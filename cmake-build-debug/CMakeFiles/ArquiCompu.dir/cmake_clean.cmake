6d115d86a0393921c334e4879c9f5f217814ee78
Milena <milelucero_09@hotmail.com>
2019-06-21 10:19:04 -0300

    Falta la velocidad y el escape

---
+++ cmake-build-debug/CMakeFiles/ArquiCompu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ArquiCompu.dir/main.c.o"
+  "CMakeFiles/ArquiCompu.dir/pruebas.c.o"
+  "ArquiCompu.pdb"
+  "ArquiCompu"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ArquiCompu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
